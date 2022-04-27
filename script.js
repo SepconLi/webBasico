432e8b458dfd5cfe63a01d1f602705792fafb4bd
Rodrigo Li Qiu <rodrigo.li@ucr.ac.cr>
2022-04-26 18:18:13 -0600

    bug fixing

---
+++ script.js
@@ -0,0 +1,6 @@
+const validarUsuario = () => {
+    var user;
+    user = document.getElementById("usuario");
+    var password = document.getElementById("password");
+    alert(user + " " + password);
+}
